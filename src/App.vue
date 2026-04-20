16d7f6ed1bc20b19ade476eb2435a940d9f878d7
threez <2378979780@qq.com>
2026-04-21 02:34:25 +0800

    引入 Element Plus 组件库

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
-<script setup>
+<template>
+  <div style="text-align: center; margin-top: 50px;">
+    <h1>我的新网站</h1>
+    <el-button type="primary">这是 Element Plus 的按钮</el-button>
+    <el-button type="success" round>圆角成功按钮</el-button>
+  </div>
+</template>
 
+<script setup>
+// 注意：这里不要写任何 import HelloWorld 的语句
 </script>
 
-<template>
-  <h1>我的新网站正在施工中...</h1>
-</template>
+<style>
+/* 简单的全局样式 */
+body {
+  margin: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
